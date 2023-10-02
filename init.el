e854c5218f2261d5d1c8f8fb6ea46fdeccc627af
David Scravaglieri <david@scravaglieri.net>
2023-10-02 10:52:01 +0200

    Initial commit with basic configuration
    Esentially we are using straight.el package manager
    
    [ticket: X]

---
+++ init.el
@@ -0,0 +1,42 @@
+
+;; we are using straight.el package manager
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; we create an alias for use-package
+(setq package-enable-at-startup nil
+      straight-use-package-by-default t)
+(straight-use-package 'use-package)
+
+;;; Backup files in backup directory
+(setq backup-directory-alist '(("." . "~/.config/emacs/backup"))
+  backup-by-copying t    ; Don't delink hardlinks
+  version-control t      ; Use version numbers on backups
+  delete-old-versions t  ; Automatically delete excess backups
+  kept-new-versions 20   ; how many of the newest versions to keep
+  kept-old-versions 5    ; and how many of the old
+  )
+
+;;; we activate line number
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
